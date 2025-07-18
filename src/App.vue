805f6dcdde1121641ceb3e4324e5b5705898b787
overlookpopular936 <crimp_scorch607@silomails.com>
2025-07-18 19:01:34 +0800

    feat: 初始化项目，添加各种组件库依赖，添加unocss

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,9 @@ onMounted(() => {
 
 <template>
   <main class="container">
-    <div class="w-full h-10 bg-blue font-bold color-gray-800">test</div>
+    <div class="w-full h-10 bg-blue font-bold color-gray-800">
+      test
+    </div>
     <div class="w-full flex items-center justify-center gap-x-4 text-4xl p-2 mt-4">
       <div class="i-vscode-icons:file-type-light-pnpm"></div>
     </div>
